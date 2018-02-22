00be3c4d00895fa2ff69fc71f71eafe45d8f4bf5
zzxcv1314 <zzxcv1314@gmail.com>
2018-02-22 14:11:13 +0900

    add comment for views.py

--- airstatusproject/airstatusapp/views.py
+++ airstatusproject/airstatusapp/views.py
@@ -15,25 +15,28 @@ from rest_framework_extensions.mixins import NestedViewSetMixin
 #from .serializers import PersonSerializer
 #from .serializers import DeviceSerializer
 
-# Create your views here.
+#user에 관한 Viewset은 설계하지 않는다. rest auth 제공 viewset사용
 
-
+#device viewset 
 class DeviceViewSet(NestedViewSetMixin, ModelViewSet):
     
     #setattr(Device, 'dkey', 'testkey')
-    queryset = Device.objects.all()
-    serializer_class = DeviceSerializer
-    filter_backends = (DjangoFilterBackend,)
-    
-    filter_fields = ('dname', 'downer', 'dlocation', 'id',)
+    queryset = Device.objects.all() #device의 모든 object를 사용한다. 
+    serializer_class = DeviceSerializer #설계해둔 Deiveserializer를 사용한다. 
+    filter_backends = (DjangoFilterBackend,) #url filtering을 위한 framework
+    filter_fields = ('dname', 'downer', 'dlocation', 'id',) 
+    #filtering 시 사용할 field들
+
     def perform_create(self, serializer):
         #serializer.save(dkey = 'abcd')
         serializer.save(downer = str(self.request.user))
+        #ViewSet이 실행되면 downer에 현재 username을 저장한다. 
 
+#airstatus viewset 
 class AirViewSet(NestedViewSetMixin, ModelViewSet):
-    queryset = airstatus.objects.all()
-    serializer_class =  AirSerializer
-
-    filter_backends = (DjangoFilterBackend,)
+    queryset = airstatus.objects.all() #airstatus의 모든 object를 사용한다. 
+    serializer_class =  AirSerializer #설계해둔 airserializer를 사용한다. 
+    filter_backends = (DjangoFilterBackend,) #url filtering을 위한 framework
     filter_fields = ('devicekey', 'pm25','pm10','temperature','id',)
+    #filtering시 사용할 field들. 
     
