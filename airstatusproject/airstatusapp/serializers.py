0d6a76d5c3761757c18624e43778d2b9f12c1357
zzxcv1314 <zzxcv1314@gmail.com>
2018-02-22 14:07:25 +0900

    comment for serializer.py

--- airstatusproject/airstatusapp/serializers.py
+++ airstatusproject/airstatusapp/serializers.py
@@ -11,22 +11,24 @@ from rest_framework.authtoken.models import Token
 from django.utils.crypto import get_random_string
 import logging
 
+#User에 관한 Serializer는 따로 설계하지 않았다. rest-auth에서 모두 제공한다. 
 
 
-
+#Device Serializer 설계. 
 class DeviceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        
-        model = Device 
-        fields = ('dname','dlocation','dkey','downer', 'id')
-        setattr(Device, 'downer', User.username)
-        print(getattr(Device,'downer'))
+        model = Device #Device model을 사용한다.  
+        fields = ('dname','dlocation','dkey','downer', 'id') #사용할 field 
+        setattr(Device, 'downer', User.username) 
+        #현재 로그인중인 User 의 username을 가져와 Device model의 downer field에 넣어준다. 
+        #print(getattr(Device,'downer'))
         #setattr(Device, 'dkey', 'testktey')
 
+#airstatus Serializer 설계 
 class AirSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = airstatus
-        fields = ('pm25', 'pm10', 'temperature','devicekey','id')
+        model = airstatus #airstatus model을 사용한다. 
+        fields = ('pm25', 'pm10', 'temperature','devicekey','id') #사용할 field 
         
 
 
